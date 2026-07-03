735fa8781c6c5904f9d7ecbb4fbe793a325297b4
Mukund Parekh <mukund.parekh@viitor.cloud>
2026-07-03 09:34:11 +0530

    Fix: drop non-standard Host line from robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,5 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: "*", allow: "/", disallow: ["/api/"] }],
     sitemap: `${base}/sitemap.xml`,
-    host: base,
   };
 }
